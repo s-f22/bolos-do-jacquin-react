e1e642dd821ecda529903154385847571eb42ff0
Samuel <samuel.silveira@sp.senai.br>
2025-09-18 09:28:02 -0300

    add componente Produtos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Cadastro from "./pages/Cadastro/Cadastro"
+import Produtos from "./pages/Produtos/Produtos"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Cadastro />} />
+        <Route path="/produtos" element={<Produtos />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Produtos/Produtos.tsx
@@ -0,0 +1,64 @@
+import Header from '../../components/Header/Header'
+import './Produtos.css'
+
+export default function Produtos() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section className="banner"></section>
+        <section className="container">
+          <h1 className="acessivel">produtos de chocoltae</h1>
+          <div className="titulo">
+            <span>Chocolate</span>
+            <hr />
+          </div>
+          <div className="cards">
+            <div className="card">
+              <img src="../assets/imgs/choc-belga.png" alt="" />
+              <h2>Chocolate Belga</h2>
+              <p>Bolo macio de chocolate, aplicado granulado que traz crocância e um sabor irresistível.</p>
+              <span>R$ 50,00/kg.</span>
+            </div>
+            <div className="card">
+              <img src="../assets/imgs/choc-ninho.png" alt="" />
+              <h2>Chocolate com Ninho</h2>
+              <p>Bolo macio de chocolate com creme de leite Ninho, um sabor irresistível.</p>
+              <span>R$ 50,00/kg.</span>
+            </div>
+            <div className="card">
+              <img src="../assets/imgs/cenoura-choc.png" alt="" />
+              <h2>Cenoura com Chocolate</h2>
+              <p>Bolo macio cenoura com cobertura de chocolate, um sabor família e irresistível.</p>
+              <span>R$ 50,00/kg.</span>
+            </div>
+            <div className="card">
+              <img src="../assets/imgs/choc-ninho-morango.png" alt="" />
+              <h2>Ninho com Morango</h2>
+              <p>Bolo macio de chocolate com recheio de leite ninho e morango. A melhor combinação para seu dia.
+              </p>
+              <span>R$ 50,00/kg.</span>
+            </div>
+            <div className="card">
+              <img src="../assets/imgs/choc-pistache.png" alt="" />
+              <h2>Chocolate com Pistache</h2>
+              <p>Bolo macio de leite Ninho, com creme delicado e crocante de amendoim para um sabor irresistível.
+              </p>
+              <span>R$ 50,00/kg.</span>
+            </div>
+            <div className="card">
+              <img src="../assets/imgs/choc-oreo.png" alt="" />
+              <h2>Chocolate com Oreo</h2>
+              <p>Bolo macio de leite Ninho, com creme delicado e crocante de amendoim para um sabor irresistível.
+              </p>
+              <span>R$ 50,00/kg.</span>
+            </div>
+          </div>
+        </section>
+        <a className="whatsapp" href="https://wa.me/5511999998888?text=Olá%20quero%20saber%20mais!" target="_blank">
+          <img src="../assets/whatsapp.png" alt="icone do whatsapp" />
+        </a>
+      </main>
+    </>
+  )
+}
